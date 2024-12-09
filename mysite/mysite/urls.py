be9a918473e7ab562bf63e9df4297925e5f03181
o0oiiiiing <parkdahyeon1114@gmail.com>
2024-12-09 15:50:27 +0900

    241209 - settings.py 한국 시간 설정

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include("polls.urls"))
+    path('polls/', include("polls.urls"))  # polls라는 앱의 urls.py
 ]
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path  # path() : URL과 뷰를 매핑하기 위한 함수
 
-from . import views
+from . import views  # 현재 앱의 views.py 파일을 가져온다.
 
+"""
+Django는 urlpatterns라는 리스트를 통해 URL과 뷰를 매핑한다.
+이 리스트에 정의된 각 path()는 특정 URL 경로를 처리한다.
+클라이언트가 route에 해당하는 요청을 보내면 연결되어 있는 view를 실행한다.
+"""
 urlpatterns = [
-    path("", views.index, name="index")    
+    path("", views.index, name="index")  # path(route, view, kwras=None, name=None)
 ]
